e71f66f51ed85e2a698adbc74a1160b9d3584634
Simcha Klein <sklein27@cs.binghamton.edu>
2022-11-08 20:32:31 -0500

    update to q3

--- bt.c
+++ bt.c
@@ -8,10 +8,10 @@ int main(); //function signature to be able to get address of main in our print_
 unsigned long* print_backtrace(unsigned int count){
 
   //allocate array to hold count number of pointers
-  unsigned long *ret_arr = (unsigned long *) malloc(count);
+  unsigned long *ret_arr = (unsigned long *) malloc(sizeof(unsigned long *) * count);
   /*find the bounds of main*/
-  unsigned long *start = (unsigned long *)&main; //start
-  unsigned long *end; //end
+  unsigned char *start = (unsigned char *)&main; //start
+  unsigned char *end; //end
   //-start by reading bytes until you hit the return function (0xc3) 
   unsigned char *curr_byte = (unsigned char *)&main;
   while(*curr_byte != 0xc3){
@@ -19,21 +19,25 @@ unsigned long* print_backtrace(unsigned int count){
     curr_byte++;
   }
   //-indentify the address of the return instruction, this will be after the current byte reads 0xc3
-  end = (unsigned long *)curr_byte; 
+  end = curr_byte; 
   //-The start address of main and the address of the return instruction form the bounds of main*/
 
   /*Gather count number of return addresses in the preceding frames, or until you hit a return address that is inside main*/
   unsigned long *curr_rbp = getRBP(); //ASM helper function to obtain rbp pointer
+  unsigned long *ret_addr;
   while (count > 0){
 
-    unsigned long *ret_addr = (unsigned long*)(curr_rbp + 8); //get current address 
+    ret_addr = (unsigned long *)*(curr_rbp + 8); //get current address 
+    ret_arr = (unsigned long *)ret_addr; //accumulate ret_addr
     printf("%p\n", ret_arr);
     ret_arr++;
 
     // start <= ret_addr <= end (Checking if ret_addr is in bounds of main)
-    if((start <= ret_addr) && (ret_addr <= end))
+    if(((unsigned long *)start <= ret_addr) && (ret_addr <= (unsigned long *)end)){
       return ret_arr;
+    }
     
+    curr_rbp = (unsigned long *)*(curr_rbp);
     count--;
   }
   return ret_arr;
